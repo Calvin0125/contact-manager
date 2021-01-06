fca0449412c002359be611b6334867380af9798f
Calvin Conley <cconley0125@gmail.com>
2021-01-06 15:01:51 -0500

    updated styles for contact list

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -12,6 +12,27 @@ let contacts = [
     email: "daffy@duck.com",
     phone_number: "12345678901",
     tags: null,
+  },
+  {
+    id: 3,
+    full_name: "Bugs Bunny",
+    email: "bugs@bunny.com",
+    phone_number: "12345678901",
+    tags: ["work", "school", "friend"],
+  },
+  {
+    id: 4,
+    full_name: "Wiley Coyote",
+    email: "wiley@coyote.com",
+    phone_number: "12345678901",
+    tags: null,
+  },
+  {
+    id: 5,
+    full_name: "Tweety Bird",
+    email: "tweety@bird.com",
+    phone_number: "12345678901",
+    tags: null,
   }
 ];
 
